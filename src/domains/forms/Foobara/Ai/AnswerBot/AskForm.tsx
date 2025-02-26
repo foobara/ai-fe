e26a92c76494d764cc4d0d8f16128907cf6f8a6d
Miles Georgi <azimux@gmail.com>
2025-02-26 11:07:15 -0800

    Regenerate AskForm

--- src/domains/forms/Foobara/Ai/AnswerBot/AskForm.tsx
+++ src/domains/forms/Foobara/Ai/AnswerBot/AskForm.tsx
@@ -9,10 +9,12 @@ import { type Error as AskError } from '../../../../Foobara/Ai/AnswerBot/Ask/Err
 
 import { type model } from '../../../../Foobara/Ai/AnswerBot/Types/model'
 
+import { type service } from '../../../../Foobara/Ai/AnswerBot/Types/service'
+
 export default function AskForm (): JSX.Element {
   const [question, setQuestion] = useState<string | undefined>(undefined)
 
-  // const [service, setService] = useState<'open-ai' | 'anthropic' | undefined>(undefined)
+  const [service, setService] = useState<service | undefined>(undefined)
 
   const [model, setModel] = useState<model | undefined>(undefined)
 
@@ -30,11 +32,11 @@ export default function AskForm (): JSX.Element {
       // TODO: perform some kind of validation error
       return
     }
-    //
-    // if (service == null) {
-    //   // TODO: perform some kind of validation error
-    //   return
-    // }
+
+    if (service == null) {
+      // TODO: perform some kind of validation error
+      return
+    }
 
     if (model == null) {
       // TODO: perform some kind of validation error
@@ -43,7 +45,7 @@ export default function AskForm (): JSX.Element {
 
     const inputs: AskInputs = {
       question,
-      // service,
+      service,
       model
     }
 
@@ -77,18 +79,18 @@ export default function AskForm (): JSX.Element {
           placeholder="question"
                   />
 
-        {/* <select */}
-        {/*  value={service ?? ''} */}
-        {/*  onChange={(e) => { setService(e.target.value as 'open-ai' | 'anthropic') }} */}
-        {/*        > */}
-        {/*  <option value="open-ai">open-ai</option><option value="anthropic">anthropic</option> */}
-        {/* </select> */}
+        <select
+          value={service ?? ''}
+          onChange={(e) => { setService(e.target.value as service) }}
+                >
+          <option value="anthropic">anthropic</option><option value="ollama">ollama</option><option value="open-ai">open-ai</option>
+        </select>
 
         <select
           value={model ?? ''}
           onChange={(e) => { setModel(e.target.value as model) }}
                 >
-          <option value="babbage-002">babbage-002</option><option value="chatgpt-4o-latest">chatgpt-4o-latest</option><option value="claude-2.0">claude-2.0</option><option value="claude-2.1">claude-2.1</option><option value="claude-3-5-haiku-20241022">claude-3-5-haiku-20241022</option><option value="claude-3-5-sonnet-20240620">claude-3-5-sonnet-20240620</option><option value="claude-3-5-sonnet-20241022">claude-3-5-sonnet-20241022</option><option value="claude-3-haiku-20240307">claude-3-haiku-20240307</option><option value="claude-3-opus-20240229">claude-3-opus-20240229</option><option value="claude-3-sonnet-20240229">claude-3-sonnet-20240229</option><option value="dall-e-2">dall-e-2</option><option value="dall-e-3">dall-e-3</option><option value="davinci-002">davinci-002</option><option value="gpt-3.5-turbo">gpt-3.5-turbo</option><option value="gpt-3.5-turbo-0125">gpt-3.5-turbo-0125</option><option value="gpt-3.5-turbo-1106">gpt-3.5-turbo-1106</option><option value="gpt-3.5-turbo-16k">gpt-3.5-turbo-16k</option><option value="gpt-3.5-turbo-instruct">gpt-3.5-turbo-instruct</option><option value="gpt-3.5-turbo-instruct-0914">gpt-3.5-turbo-instruct-0914</option><option value="gpt-4">gpt-4</option><option value="gpt-4-0125-preview">gpt-4-0125-preview</option><option value="gpt-4-0613">gpt-4-0613</option><option value="gpt-4-1106-preview">gpt-4-1106-preview</option><option value="gpt-4-turbo">gpt-4-turbo</option><option value="gpt-4-turbo-2024-04-09">gpt-4-turbo-2024-04-09</option><option value="gpt-4-turbo-preview">gpt-4-turbo-preview</option><option value="gpt-4o">gpt-4o</option><option value="gpt-4o-2024-05-13">gpt-4o-2024-05-13</option><option value="gpt-4o-2024-08-06">gpt-4o-2024-08-06</option><option value="gpt-4o-2024-11-20">gpt-4o-2024-11-20</option><option value="gpt-4o-audio-preview">gpt-4o-audio-preview</option><option value="gpt-4o-audio-preview-2024-10-01">gpt-4o-audio-preview-2024-10-01</option><option value="gpt-4o-audio-preview-2024-12-17">gpt-4o-audio-preview-2024-12-17</option><option value="gpt-4o-mini">gpt-4o-mini</option><option value="gpt-4o-mini-2024-07-18">gpt-4o-mini-2024-07-18</option><option value="gpt-4o-mini-audio-preview">gpt-4o-mini-audio-preview</option><option value="gpt-4o-mini-audio-preview-2024-12-17">gpt-4o-mini-audio-preview-2024-12-17</option><option value="gpt-4o-mini-realtime-preview">gpt-4o-mini-realtime-preview</option><option value="gpt-4o-mini-realtime-preview-2024-12-17">gpt-4o-mini-realtime-preview-2024-12-17</option><option value="gpt-4o-realtime-preview">gpt-4o-realtime-preview</option><option value="gpt-4o-realtime-preview-2024-10-01">gpt-4o-realtime-preview-2024-10-01</option><option value="gpt-4o-realtime-preview-2024-12-17">gpt-4o-realtime-preview-2024-12-17</option><option value="o1-mini">o1-mini</option><option value="o1-mini-2024-09-12">o1-mini-2024-09-12</option><option value="o1-preview">o1-preview</option><option value="o1-preview-2024-09-12">o1-preview-2024-09-12</option><option value="omni-moderation-2024-09-26">omni-moderation-2024-09-26</option><option value="omni-moderation-latest">omni-moderation-latest</option><option value="text-embedding-3-large">text-embedding-3-large</option><option value="text-embedding-3-small">text-embedding-3-small</option><option value="text-embedding-ada-002">text-embedding-ada-002</option><option value="tts-1">tts-1</option><option value="tts-1-1106">tts-1-1106</option><option value="tts-1-hd">tts-1-hd</option><option value="tts-1-hd-1106">tts-1-hd-1106</option><option value="whisper-1">whisper-1</option>
+          <option value="babbage-002">babbage-002</option><option value="chatgpt-4o-latest">chatgpt-4o-latest</option><option value="claude-2.0">claude-2.0</option><option value="claude-2.1">claude-2.1</option><option value="claude-3-5-haiku-20241022">claude-3-5-haiku-20241022</option><option value="claude-3-5-sonnet-20240620">claude-3-5-sonnet-20240620</option><option value="claude-3-5-sonnet-20241022">claude-3-5-sonnet-20241022</option><option value="claude-3-7-sonnet-20250219">claude-3-7-sonnet-20250219</option><option value="claude-3-haiku-20240307">claude-3-haiku-20240307</option><option value="claude-3-opus-20240229">claude-3-opus-20240229</option><option value="claude-3-sonnet-20240229">claude-3-sonnet-20240229</option><option value="dall-e-2">dall-e-2</option><option value="dall-e-3">dall-e-3</option><option value="davinci-002">davinci-002</option><option value="gpt-3.5-turbo">gpt-3.5-turbo</option><option value="gpt-3.5-turbo-0125">gpt-3.5-turbo-0125</option><option value="gpt-3.5-turbo-1106">gpt-3.5-turbo-1106</option><option value="gpt-3.5-turbo-16k">gpt-3.5-turbo-16k</option><option value="gpt-3.5-turbo-instruct">gpt-3.5-turbo-instruct</option><option value="gpt-3.5-turbo-instruct-0914">gpt-3.5-turbo-instruct-0914</option><option value="gpt-4">gpt-4</option><option value="gpt-4-0125-preview">gpt-4-0125-preview</option><option value="gpt-4-0613">gpt-4-0613</option><option value="gpt-4-1106-preview">gpt-4-1106-preview</option><option value="gpt-4-turbo">gpt-4-turbo</option><option value="gpt-4-turbo-2024-04-09">gpt-4-turbo-2024-04-09</option><option value="gpt-4-turbo-preview">gpt-4-turbo-preview</option><option value="gpt-4o">gpt-4o</option><option value="gpt-4o-2024-05-13">gpt-4o-2024-05-13</option><option value="gpt-4o-2024-08-06">gpt-4o-2024-08-06</option><option value="gpt-4o-2024-11-20">gpt-4o-2024-11-20</option><option value="gpt-4o-audio-preview">gpt-4o-audio-preview</option><option value="gpt-4o-audio-preview-2024-10-01">gpt-4o-audio-preview-2024-10-01</option><option value="gpt-4o-audio-preview-2024-12-17">gpt-4o-audio-preview-2024-12-17</option><option value="gpt-4o-mini">gpt-4o-mini</option><option value="gpt-4o-mini-2024-07-18">gpt-4o-mini-2024-07-18</option><option value="gpt-4o-mini-audio-preview">gpt-4o-mini-audio-preview</option><option value="gpt-4o-mini-audio-preview-2024-12-17">gpt-4o-mini-audio-preview-2024-12-17</option><option value="gpt-4o-mini-realtime-preview">gpt-4o-mini-realtime-preview</option><option value="gpt-4o-mini-realtime-preview-2024-12-17">gpt-4o-mini-realtime-preview-2024-12-17</option><option value="gpt-4o-realtime-preview">gpt-4o-realtime-preview</option><option value="gpt-4o-realtime-preview-2024-10-01">gpt-4o-realtime-preview-2024-10-01</option><option value="gpt-4o-realtime-preview-2024-12-17">gpt-4o-realtime-preview-2024-12-17</option><option value="llama3:8b">llama3:8b</option><option value="o1-mini">o1-mini</option><option value="o1-mini-2024-09-12">o1-mini-2024-09-12</option><option value="o1-preview">o1-preview</option><option value="o1-preview-2024-09-12">o1-preview-2024-09-12</option><option value="omni-moderation-2024-09-26">omni-moderation-2024-09-26</option><option value="omni-moderation-latest">omni-moderation-latest</option><option value="smollm2:135m">smollm2:135m</option><option value="text-embedding-3-large">text-embedding-3-large</option><option value="text-embedding-3-small">text-embedding-3-small</option><option value="text-embedding-ada-002">text-embedding-ada-002</option><option value="tts-1">tts-1</option><option value="tts-1-1106">tts-1-1106</option><option value="tts-1-hd">tts-1-hd</option><option value="tts-1-hd-1106">tts-1-hd-1106</option><option value="whisper-1">whisper-1</option>
         </select>
 
         <button onClick={run}>Ask</button>
